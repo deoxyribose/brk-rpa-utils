39c7b3578d39c77e8dc769eb9492a60cdf99e0cf
Dominik Zdyb <dominik2711@gmail.com>
2023-12-20 13:37:29 +0100

    deleted main() to show it is meant to be used as a pure library. Made _get_credentials an internal function. corrected function arguments.

--- src/brk_rpa_utils/__init__.py
+++ src/brk_rpa_utils/__init__.py
@@ -1,3 +1,4 @@
 # SPDX-FileCopyrightText: 2023-present Dominik Zdyb Lillemaehlum <Dominik.Zdyb.Lillemaehlum@brk.dk>
 #
 # SPDX-License-Identifier: MIT
+from .main import start_opus, start_ri
--- src/brk_rpa_utils/main.py
+++ src/brk_rpa_utils/main.py
@@ -1,23 +1,41 @@
-import getpass
-import json
+"""
 import os
-import subprocess
-import time
-from pathlib import Path
-
-import win32com.client  # pywin32
+import getpass
 from dotenv import load_dotenv  # python-dotenv
-from loguru import logger as log
-from playwright.sync_api import Playwright
 
 load_dotenv()
 sapshcut_path = Path(os.getenv("SAPSHCUT_PATH"))
 pam_path = os.getenv("PAM_PATH")
 ri_path = os.getenv("RI_PATH")
 robot_name = getpass.getuser()
+"""
+
+import json
+import subprocess
+import time
+from pathlib import Path
 
 
-def get_credentials(robot_name, fagsystem):
+import win32com.client  # pywin32
+from loguru import logger as log
+from playwright.sync_api import Playwright
+
+
+def _get_credentials(pam_path, robot_name, fagsystem):
+    """
+    Internal function to retrieve credentials.
+
+    Define pam_path in an .env file in the root of your project. Add paths like so:
+    SAPSHCUT_PATH=C:/Program Files (x86)/SAP/FrontEnd/SAPgui/sapshcut.exe
+
+    Under the pam_path uri der should be a robot_name.json file with the structure:
+
+    {
+    "ad": { "username": "robot00X", "password": "x" },
+    "opus": { "username": "jrrobot00X", "password": "x" },
+    "rollebaseretindgang": { "username": "jrrobot00X", "password": "x" }
+    }
+    """
     pass_file = Path(pam_path) / robot_name / f"{robot_name}.json"
 
     try:
@@ -39,21 +57,21 @@ def get_credentials(robot_name, fagsystem):
     return None, None
 
 
-def start_opus(robot_name):
+def start_opus(pam_path, robot_name, sapshcut_path):
     """
     Starts Opus using sapshcut.exe and credentials from PAM.
 
-    The robot00X.json file has the structure:
+    The robot_name.json file should have the structure:
 
     {
-    "ad": { "username": "robot00X", "password": "x" },
-    "opus": { "username": "jrrobot00X", "password": "x" },
-    "rollebaseretindgang": { "username": "jrrobot00X", "password": "x" }
+    "ad": { "username": "robot_name", "password": "x" },
+    "opus": { "username": "robot_name", "password": "x" },
+    "rollebaseretindgang": { "username": "robot_name", "password": "x" }
     }
     """
 
     # unpacking
-    username, password = get_credentials(robot_name, fagsystem="opus")
+    username, password = _get_credentials(pam_path, robot_name, fagsystem="opus")
 
     if not username or not password:
         log.error("Failed to retrieve credentials for robot", exc_info=True)
@@ -82,8 +100,21 @@ def start_opus(robot_name):
         return None
 
 
-def start_ri(playwright: Playwright) -> None:
-    username, password = get_credentials(robot_name, fagsystem="rollebaseretindgang")
+def start_ri(pam_path, robot_name, ri_url, playwright: Playwright) -> None:
+    """
+    Starts s browser pointed to ri_url (fx https://portal.kmd.dk/irj/portal)
+    and logs into Rollebaseret Indgang (RI) using  and credentials from PAM.
+
+    The robot_name.json file should have the structure:
+
+    {
+    "ad": { "username": "robot_name", "password": "x" },
+    "opus": { "username": "robot_name", "password": "x" },
+    "rollebaseretindgang": { "username": "robot_name", "password": "x" }
+    }
+    """
+
+    username, password = _get_credentials(pam_path, robot_name, fagsystem="rollebaseretindgang")
 
     if not username or not password:
         log.error("Failed to retrieve credentials for robot", exc_info=True)
@@ -93,7 +124,7 @@ def start_ri(playwright: Playwright) -> None:
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context(viewport={"width": 2560, "height": 1440})
         page = context.new_page()
-        page.goto(ri_path)
+        page.goto(ri_url)
         page.get_by_placeholder("Brugernavn").click()
         page.get_by_placeholder("Brugernavn").fill(username)
         page.get_by_placeholder("Brugernavn").press("Tab")
@@ -107,13 +138,3 @@ def start_ri(playwright: Playwright) -> None:
     except Exception:
         log.error("An error occurred while logging into the portal", exc_info=True)
         return None
-
-
-def main():
-    get_credentials(robot_name)
-    start_opus(robot_name)
-    start_ri()
-
-
-if __name__ == "__main__":
-    main()
