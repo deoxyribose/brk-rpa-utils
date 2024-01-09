d201bb4b9c6524cf286d1c17b522af30b1d1803f
Dominik Zdyb <dominik2711@gmail.com>
2024-01-09 12:32:27 +0100

    added func to dunder init

--- src/brk_rpa_utils/__init__.py
+++ src/brk_rpa_utils/__init__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2023-present Dominik Zdyb Lillemaehlum <Dominik.Zdyb.Lillemaehlum@brk.dk>
 #
 # SPDX-License-Identifier: MIT
-from .main import start_opus, start_ri
+from .main import start_opus, start_ri, parse_ri_html_report_to_dataframe
