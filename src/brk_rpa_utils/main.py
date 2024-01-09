2748719bdca3626aec321ab793308084b012d2c4
Dominik Zdyb <dominik2711@gmail.com>
2024-01-09 11:25:36 +0100

    added parse_ri_html_report_to_dataframe

--- src/brk_rpa_utils/main.py
+++ src/brk_rpa_utils/main.py
@@ -2,6 +2,12 @@
 import os
 import getpass
 from dotenv import load_dotenv  # python-dotenv
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import io
+from pathlib import Path
+from loguru import logger
 
 load_dotenv()
 sapshcut_path = Path(os.getenv("SAPSHCUT_PATH"))
@@ -137,4 +143,56 @@ def start_ri(pam_path, robot_name, ri_url, playwright: Playwright) -> None:
 
     except Exception:
         log.error("An error occurred while logging into the portal", exc_info=True)
+        return None
+
+
+def parse_ri_html_report_to_dataframe(mhtml_path) -> None:
+    """
+    Parses an mhtml file downloaded from Rollobaseret Indgang.
+    The default download calls the file xls, but it is a kind of html file.
+
+    ## Usage
+    mhtml_path = Path(folder_data_session / 'test.html')
+
+    df_mhtml = parse_ri_html_report_to_dataframe(mhtml_path)
+    """
+    try:
+        # Read MHTML file
+        with open(mhtml_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+
+        # Find the HTML part of the file
+        match = re.search(r'<html.*<\/html>', content, re.DOTALL)
+        if not match:
+            raise ValueError("No HTML content found in the file")
+        html_content = match.group(0)
+
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(html_content, 'lxml')
+
+        # Find all tables within the parsed HTML
+        tables = soup.find_all('table')
+        if not tables:
+            raise ValueError("No tables found in the HTML content")
+
+        # Select the largest table based on character count
+        largest_table = max(tables, key=lambda table: len(str(table)))
+
+        # Convert the largest HTML table to a pandas DataFrame
+        df_mhtml = pd.read_html(io.StringIO(str(largest_table)), decimal=',', thousands='.', header=None)
+        if not df_mhtml:
+            raise ValueError("Failed to parse the largest table into a DataFrame")
+
+        df_mhtml = df_mhtml[0]
+        df_mhtml.columns = df_mhtml.iloc[0]
+        df_mhtml = df_mhtml.drop(0)
+        df_mhtml.reset_index(drop=True, inplace=True)
+        df_mhtml.rename(columns={'Slut F-periode': 'date', 'Lønart': 'lonart', 'Antal': 'antal'}, inplace=True)
+
+        # Convert 'date' column to datetime
+        df_mhtml['date'] = pd.to_datetime(df_mhtml['date'], format='%d%m%Y')
+        return df_mhtml
+
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
         return None
